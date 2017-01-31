45a9a2670dd1e17c9a0ff375ab4d8bc45a82365c
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2017-01-31 16:37:43 -0600

    These are generated dynamically now

--- today.php
+++
@@ -1,6 +0,0 @@
-<?php
-date_default_timezone_set(timezone_name_from_abbr("CST"));
-$date = date('Y-m-d');
-echo "<pre>";
-echo htmlspecialchars(file_get_contents( $date . ".log" )); // get the contents, and echo it out.
-?>
