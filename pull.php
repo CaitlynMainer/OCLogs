b3215710caf6d8e6b5bbd4161899d062fc43496d
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2017-01-31 16:27:51 -0600

    merp?

--- pull.php
+++ pull.php
@@ -1,4 +1,13 @@
 <?PHP
 //Test
-    echo shell_exec("/home/znc/.znc/moddata/log/neo/esper/#oc/gitpull.sh");
+    $cmd = "/home/znc/.znc/moddata/log/neo/esper/#oc/gitpull.sh";
+    while (@ ob_end_flush()); // end all output buffers if any
+
+    $proc = popen($cmd, 'r');
+    echo '<pre>';
+    while (!feof($proc)) {
+        echo fread($proc, 4096);
+        @ flush();
+    }
+    echo '</pre>';
 ?>
