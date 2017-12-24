1057caacd588a0e6d9466ea34561f2f0688c9092
Caitlyn <Michiyo.Ravencroft@gmail.com>
2017-12-23 21:11:16 -0600

    Switch Index to new MySQL base

--- index.php
+++ index.php
@@ -1,52 +1,58 @@
-<?php
+<?PHP
+include("config.php");
+// Create connection
+$mysqli = new mysqli($config["mysql_server"], $config["mysql_user"], $config["mysql_pass"], $config["mysql_db"]);
+if (mysqli_connect_errno()) {
+	die("Failed to connect to MySQL: " . mysqli_connect_error());
+}
+?>
+<!doctype html>
+<html>
 
-    // Include the DirectoryLister class
-    require_once('resources/DirectoryLister.php');
+<head>
+  <meta charset="UTF-8">
+  <title>OCLogs</title>
+  <link rel="stylesheet" href=".style.css">
+  <script src=".sorttable.js"></script>
+</head>
 
-    // Initialize the DirectoryLister object
-    $lister = new DirectoryLister();
+<body>
 
-    // Restrict access to current directory
-    ini_set('open_basedir', getcwd());
+  <div id="container">
+  
+    <h1>OCLogs listing</h1>
+    <?PHP
+date_default_timezone_set(timezone_name_from_abbr("CST"));
+$yest = date('Y-m-d', strtotime( '-1 days' ));
+$today = date('Y-m-d');
 
-    // Return file hash
-    if (isset($_GET['hash'])) {
+?>
+<a href="stats.html">Stats</a> | <a href=".getlatest.php">Download All (.tgz)</a> | <a href="parser.php?log=<?PHP echo $yest; ?>.log">Yesterday's Logs</a> | <a href="parser.php?log=<?PHP echo $today; ?>.log">Today's Logs</a> | <form action="search" method="get" style="margin: 0; padding: 0; display:inline;"><input type="hidden" name="case" value="1" /><input style="display: inline;" type="text" name="search"><input style="display: inline;" type="submit" value="Search"></form>
+    <table class="sortable">
+      <thead>
+        <tr>
+          <th>Filename</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php
+$stmt = $mysqli->prepare("SELECT DISTINCT `date` FROM `logs` WHERE `channel`='#oc' ORDER BY `date` DESC");
+$stmt->execute();
+    $stmt->bind_result($date);
 
-        // Get file hash array and JSON encode it
-        $hashes = $lister->getFileHash($_GET['hash']);
-        $data   = json_encode($hashes);
-
-        // Return the data
-        die($data);
-
-    }
-
-    if (isset($_GET['zip'])) {
-
-        $dirArray = $lister->zipDirectory($_GET['zip']);
-
-    } else {
-
-        // Initialize the directory array
-        if (isset($_GET['dir'])) {
-            $dirArray = $lister->listDirectory($_GET['dir']);
-        } else {
-            $dirArray = $lister->listDirectory('.');
-        }
-
-        // Define theme path
-        if (!defined('THEMEPATH')) {
-            define('THEMEPATH', $lister->getThemePath());
-        }
-
-        // Set path to theme index
-        $themeIndex = $lister->getThemePath(true) . '/index.php';
-
-        // Initialize the theme
-        if (file_exists($themeIndex)) {
-            include($themeIndex);
-        } else {
-            die('ERROR: Failed to initialize theme');
-        }
-
-    }
+    $sqltime = (microtime(true) - $time_start);
+    /* fetch value */
+    $i = 0;
+    while ($stmt->fetch()) {
+          print("
+          <tr class='$class'>
+            <td><a href='/parser?log=$date.log'>$date.log</a></td>
+          </tr>");
+          }
+          $stmt->close();
+      ?>
+      </tbody>
+    </table>
+  </div>
+</body>
+</html>
