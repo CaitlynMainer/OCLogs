7b242f912c9fb1a906d35c58ff8341b276e0bba8
Caitlyn Mainer <michiyo.ravencroft@gmail.com>
2017-12-26 08:28:46 -0600

    remove fancybox

--- resources/themes/bootstrap/default_header.php
+++
@@ -1,7 +0,0 @@
-<?PHP
-date_default_timezone_set(timezone_name_from_abbr("CST"));
-$yest = date('Y-m-d', strtotime( '-1 days' ));
-$today = date('Y-m-d');
-
-?>
-<a href="stats.html">Stats</a> | <a href=".getlatest.php">Download All (.tgz)</a> | <a href="parser.php?log=<?PHP echo $yest; ?>.log">Yesterday's Logs</a> | <a href="parser.php?log=<?PHP echo $today; ?>.log">Today's Logs</a> | <form action="search" method="get" style="margin: 0; padding: 0; display:inline;"><input type="hidden" name="case" value="1" /><input style="display: inline;" type="text" name="search"><input style="display: inline;" type="submit" value="Search"></form>
