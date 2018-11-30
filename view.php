603bfcde604870d5a335ee816e30d2c29e97aaec
Caitlyn <Michiyo.Ravencroft@gmail.com>
2018-11-30 13:25:11 -0800

    Update view.php

--- view.php
+++ view.php
@@ -154,6 +154,10 @@ if ($escaped != "") {
             if (!isset($_GET['plain'])) {
               $line = $mcp->colorize($line);
               list($line, $type) = parseLine($line, $line_number);
+              $line = str_replace("&lt;Corded&gt; &lt;", "<img height=\"16\" width=\"16\" src=\"https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png\" title=\"<Corded> \">&lt;", $line);
+              $line = str_replace("&lt;Discord&gt; &lt;", "<img height=\"16\" width=\"16\" src=\"https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png\" title=\"<Discord> \">&lt;", $line);
+              $line = str_replace("&lt;MrConductor&gt; &lt;", "<img height=\"16\" width=\"16\" src=\"https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png\" title=\"<MrConductor> \">&lt;", $line);
+              $line = str_replace("&lt;MrConductor1&gt; &lt;", "<img height=\"16\" width=\"16\" src=\"https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png\" title=\"<MrConductor> \">&lt;", $line);
             }
             if (isset($_GET['linenum'])) {
               $line = "$line_number: " . $line;
@@ -161,10 +165,6 @@ if ($escaped != "") {
             if (!isset($_GET['nolinks'])) {
               $line = makeClickableLinks($line);
             }
-            $line = str_replace("&lt;Corded&gt; &lt;", "<img height=\"16\" width=\"16\" src=\"https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png\" title=\"<Corded> \">&lt;", $line);
-            $line = str_replace("&lt;Discord&gt; &lt;", "<img height=\"16\" width=\"16\" src=\"https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png\" title=\"<Discord> \">&lt;", $line);
-            $line = str_replace("&lt;MrConductor&gt; &lt;", "<img height=\"16\" width=\"16\" src=\"https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png\" title=\"<MrConductor> \">&lt;", $line);
-            $line = str_replace("&lt;MrConductor1&gt; &lt;", "<img height=\"16\" width=\"16\" src=\"https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png\" title=\"<MrConductor> \">&lt;", $line);
             $buffer .= "<div id='CL$line_number' class='full_line $type'>" . $line . "</div>";
             $line_number++;
             $buffer .= "<script>$('#pageContent').css('height', $(window).height() - 20); lineNum = $line_number;</script>";
